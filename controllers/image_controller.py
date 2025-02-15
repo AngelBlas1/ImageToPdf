1ed89ecf57834ba3e064b0f44d6abd6c80638024
Angel Blas <dblas962@gmail.com>
2025-02-15 16:29:29 -0600

    Update the image_controller for image_service

--- controllers/image_controller.py
+++ controllers/image_controller.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, UploadFile
+from services.image_service import ImageService
 
 router = APIRouter()
+image_service = ImageService()
 
 @router.post("/upload/")
 async def upload_file(
     file: UploadFile,
     name: str,
     email: str
-): return {"filename": file.filename, "name": name, "email": email}
+): return image_service.process_image(file, name, email)
